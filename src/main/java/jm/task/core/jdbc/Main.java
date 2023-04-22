f73c1d534bb2d27c1a06c9aa45028b2feab027ca
Alexey Kirienko <alkirienkoeps1@gmail.com>
2023-04-22 20:01:20 +0300

    commit 2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,33 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
 
-        UserDao userDao = new UserDaoJDBCImpl();
+        User user1 = new User("Name1", "LastName1", (byte) 20);
+        User user2 = new User("Name2", "LastName2", (byte) 25);
+        User user3 = new User("Name3", "LastName3", (byte) 31);
+        User user4 = new User("Name4", "LastName4", (byte) 38);
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
 
-        userDao.removeUserById(1);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+//        System.out.println(userService.getAllUsers());
+
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user.toString());
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
 
